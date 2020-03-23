d29983440e5667756094218d7c616a85eff79d7d
Jaime Feldman <jamiefel@gmail.com>
2020-03-22 22:57:45 -0300

    Imprimeindo el pid del proceso de la app.

--- holaMundo.m
+++ holaMundo.m
@@ -1,6 +1,15 @@
 #import <Foundation/Foundation.h>
+#import <unistd.h>
+#import <sys/types.h>
+#import <stdlib.h>
+
 int main(void) {
 
 	NSLog(@"hola mundo desde objective-c!\n");
+	pid_t pid;
+	pid = getpid();
+	printf("pid de la app: %d\n", pid);
+
+	
 	return 0;
 }
